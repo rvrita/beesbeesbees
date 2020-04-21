ea4a18986d3fb4c73fb6ebe0204ec5fcb253b111
Rita Rozsavolgyi <rvrita.me@gmail.com>
2020-04-20 20:05:41 -0700

    Complete Bee

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,12 @@
 var Bee = function() {
+  // reference to Grub superclass
+  Grub.call(this);
+  this.age = 5;
+  this.color = 'yellow';
+  this.job = 'keep on growing';
 };
 
+// inherit properties from superclass
+Bee.prototype = Object.create(Grub.prototype);
+// setting constructor
+Bee.prototype.constructor = Bee;
